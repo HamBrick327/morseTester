6b364b262b3545985200fc77a2bf7ca2b6ad6fe2
HamBrick327 <92757936+HamBrick327@users.noreply.github.com>
2023-12-19 23:00:40 -0700

    added the basic stuff, now need to get logic and time working

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
 import pygame
+from pygame import freetype
+from time import time, sleep
 
 pygame.init()
 
+window = pygame.display.set_mode((500, 500))
 
-window = pygame.display.set_mode(500, 500)
+keydown = False
+main = True
+while main:
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+            print("space pressed!")
+            keydown = True
+            start = time()
+        if event.type == pygame.QUIT:
+            exit()
+        if event.type == pygame.KEYUP and event.key == pygame.K_SPACE:
+            print("space up")
+            keydown = False
+            print("key down for " + start - time())
+
+            

2373b2f6c2431aa744866b85f53c1dd1932d3eef
HamBrick327 <derpykidgamer@gmail.com>
2023-12-21 15:29:04 -0700

    forgot to add main.py

--- main.py
+++ main.py
@@ -2,23 +2,69 @@ import pygame
 from pygame import freetype
 from time import time, sleep
 
+## import os
+
 pygame.init()
 
 window = pygame.display.set_mode((500, 500))
 
+morsecode = {'.-': 'A',
+'-...': 'B',
+'-.-.': 'C',
+'-..': 'D',
+'.': 'E',
+'..-.': 'F',
+'--.': 'G',
+'....': 'H',
+'..': 'I',
+'.---': 'J',
+'-.-': 'K',
+'.-..': 'L',
+'--': 'M',
+'-.': 'N',
+'---': 'O',
+'.--.': 'P',
+'--.-': 'Q',
+'.-.': 'R',
+'...': 'S',
+'-': 'T',
+'..-': 'U',
+'...-': 'V',
+'.--': 'W',
+'-..-': 'X',
+'-.--': 'Y',
+'--..': 'Z',
+'-----': '0',
+'.----': '1',
+'..---': '2',
+'...--': '3',
+'....-': '4',
+'.....': '5',
+'-....': '6',
+'--...': '7',
+'---..': '8',
+'----.': '9'}
+
+font = freetype.SysFont('Arial', 50)
+
+print(morsecode[".-"])
+
 keydown = False
-main = True
+main = False
 while main:
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-            print("space pressed!")
             keydown = True
             start = time()
         if event.type == pygame.QUIT:
             exit()
         if event.type == pygame.KEYUP and event.key == pygame.K_SPACE:
-            print("space up")
             keydown = False
-            print("key down for " + start - time())
+            timedown = time() - start
+            ## logic for determining if . or -
+            if timedown > 0.2:
+                print("-")
+            elif timedown < 0.2:
+                print(".")
 
             
